8b6fd4681762a856bc284f4e37732657fa53af8b
João Vitor <joao.coutinho01@outlook.com>
2026-07-20 18:31:25 -0300

    ci: fix yaml file with urls

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('cypress')
-require('dotenv').config()
+const path = require('path')
+
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 const maxRetries = parseInt(process.env.SERVEREST_MAX_RETRIES, 10) || 3
 
