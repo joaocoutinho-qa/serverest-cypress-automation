343a3ecbd802a4051c9ca92da9f43001eea8cc5a
João Vitor <joao.coutinho01@outlook.com>
2026-07-20 18:19:51 -0300

    tests: last updates, ready to run

--- cypress/e2e/front/shoppingList.cy.js
+++ cypress/e2e/front/shoppingList.cy.js
@@ -5,50 +5,64 @@ const shoppingListPage = Pages.ShoppingListPage
 describe('Shopping List Frontend Tests', () => {
   beforeEach(() => {
     shoppingListPage.setupInterceptors()
+    cy.createAuthenticatedUser(userData.normalUser)
+    cy.createAuthenticatedUser(userData.adminUser)
+    cy.createProductsFromFixture()
     cy.uiLoginUser(userData.normalUser.email, userData.normalUser.password)
   })
 
-  it('Search for product and validate search results', () => {
-    const mockedProduct = Cypress.env('createdProducts')[0]
+  afterEach(() => {
+    cy.cleanupTestData()
+  })
 
-    shoppingListPage.searchProduct(mockedProduct.nome)
-    shoppingListPage.validateSearchResults(mockedProduct.nome)
+  it('Search for product and validate search results', () => {
+    Cypress.env('createdProducts')
+    cy.get('@products').then((products) => {
+      const createdProduct = products[0]
+      shoppingListPage.searchProduct(createdProduct.nome)
+      shoppingListPage.validateSearchResults(createdProduct.nome)
+    })
   })
 
   it('Add single product to shopping list and validate persistence', () => {
-    const mockedProduct = Cypress.env('createdProducts')[0]
-
-    shoppingListPage.searchProduct(mockedProduct.nome)
-    shoppingListPage.addProductToShoppingList()
-    shoppingListPage.validateProductInShoppingList(mockedProduct.nome, mockedProduct.preco)
-    shoppingListPage.getCartItemCount().should('equal', 1)
+    cy.get('@products').then((products) => {
+      const createdProduct = products[0]
+      shoppingListPage.searchProduct(createdProduct.nome)
+      shoppingListPage.addProductToShoppingList()
+      shoppingListPage.validateProductInShoppingList(createdProduct.nome, createdProduct.preco)
+      shoppingListPage.getCartItemCount().should('equal', 1)
+    })
   })
 
-  it('Add multiple products and clear entire cart', () => {
-    const [product1, product2] = Cypress.env('createdProducts')
+  it('Add multiple products and clear shopping list', () => {
+    cy.get('@products').then((products) => {
+      const product1 = products[0]
+      const product2 = products[1];
+      
+      [product1, product2].forEach((product) => {
+        shoppingListPage.navigateToHome()
+        shoppingListPage.searchProduct(product.nome)
+        shoppingListPage.addProductToShoppingList()
+      })
 
-    [product1, product2].forEach((product) => {
-      shoppingListPage.navigateToHome()
-      shoppingListPage.searchProduct(product.nome)
-      shoppingListPage.addProductToShoppingList()
+      shoppingListPage.navigateToShoppingList()
+      shoppingListPage.clearShoppingList()
+      shoppingListPage.validateEmptyList()
     })
-
-    shoppingListPage.navigateToShoppingList()
-    shoppingListPage.clearShoppingList()
-    shoppingListPage.validateEmptyList()
   })
 
   it('Update product quantity in cart and validate price', () => {
-    const mockedProduct = Cypress.env('createdProducts')[0]
+    cy.get('@products').then((products) => {
+      const createdProduct = products[0]
+      shoppingListPage.searchProduct(createdProduct.nome)
+      shoppingListPage.addProductToShoppingList()
 
-    shoppingListPage.searchProduct(mockedProduct.nome)
-    shoppingListPage.addProductToShoppingList()
+      shoppingListPage.navigateToShoppingList()
+      shoppingListPage.validateProductQuantity(0, '1')
 
-    shoppingListPage.navigateToShoppingList()
-    shoppingListPage.validateProductQuantity(0, '1')
-
-    shoppingListPage.incrementProductQuantity(0)
-    shoppingListPage.validateProductQuantity(0, '2')
-    shoppingListPage.validateProductPrice(mockedProduct.preco * 2)
+      shoppingListPage.incrementProductQuantity(0)
+      shoppingListPage.validateProductQuantity(0, '2')
+      shoppingListPage.validateProductPrice(createdProduct.preco*2)
+    })
   })
 })
